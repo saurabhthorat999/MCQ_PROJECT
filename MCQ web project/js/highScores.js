cdfe0e313497eda5b1ba4d3fa75622086a114604
Saurabh <66156631+saurabhthorat999@users.noreply.github.com>
2021-03-16 15:42:30 +0530

    Delete MCQ web project directory

--- MCQ web project/js/highScores.js
+++
@@ -1,7 +0,0 @@
-const highScoresList = document.getElementById('highScoresList');
-
-const highScores = JSON.parse(localStorage.getItem('highScores'));
-
-highScoresList.innerHTML = highScores.map( score =>
-    ` <li>${score.name} - ${score.score} </li>`
-).join("");
--- MCQ web project/js/instructions.js
+++
@@ -1,4 +0,0 @@
-const tick = document.getElementById("formtick");
-const nextbtn = document.getElementById("nextPage");
-
-console.log(nextbtn.parentElement.classList.add("dis"));
